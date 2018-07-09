bdf75c4d5f6d3bb51944ffb57bd5f802590332c5
YangHaoRan <1064137887@qq.com>
2018-07-09 14:17:30 +0800

    【简要描述】：创建工程 【详细描述】： 	1.创建工程 	2.使用CSharp编写服务端以及客户端 	3.Siki老师视频第7集 【修 改 人】：杨浩然

---
+++ TCP客户端/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCP客户端
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("192.168.0.101"), 7800));
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+            string msg = Encoding.UTF8.GetString(data);
+            Console.WriteLine(msg);
+
+            string str = Console.ReadLine();
+            clientSocket.Send(Encoding.UTF8.GetBytes(str));
+
+            Console.ReadKey();
+            clientSocket.Close();
+        }
+    }
+}
---
+++ TCP服务器端/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCP服务器端
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.101");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 7800);
+            serverSocket.Bind(ipEndPoint);//绑定IP和端口号
+            serverSocket.Listen(0);//开始监听端口号，0代表排队数量不限制
+            Socket clientSocket = serverSocket.Accept();//接收一个客户端
+
+            //向客户端发送一条消息
+            string msg = "Hello Client ! 你好客户端";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            //接收客户端一条消息
+            byte[] dataBuffer = new byte[1024];
+            int dataLen = clientSocket.Receive(dataBuffer);
+            string msgReceive = System.Text.Encoding.UTF8.GetString(dataBuffer,0,dataLen);
+            Console.WriteLine(msgReceive);
+
+            Console.ReadKey();
+            clientSocket.Close();//断开与客户端的连接，并不是关闭客户端
+            serverSocket.Close();//关闭自身的连接
+
+        }
+    }
+}
