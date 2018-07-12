973f3e5ad37645ee47c21a284403be68e0d1dba1
YangHaoRan <1064137887@qq.com>
2018-07-12 16:30:28 +0800

    【简要描述】：数据库查询/插入 【详细描述】： 	1.使用CSharp调用sql接口读取数据库数据 	2.插入新数据 	3.siki老师视频第20集 【修 改 人】：杨浩然

--- MySQL数据库操作/Program.cs
+++ MySQL数据库操作/Program.cs
@@ -12,24 +12,37 @@ namespace MySQL数据库操作
         static void Main(string[] args)
         {
             //sql连接字符串
-            string sqlConStr = "Database = sikiedu;Data Source=127.0.0.1;port=3306;User Id=root;Password=123456";
+            string sqlConStr = "Database = sikiedu;Data Source = 127.0.0.1;port = 3306;user id= root;Password = 123456";
             MySqlConnection sqlConnection = new MySqlConnection(sqlConStr);
 
             sqlConnection.Open();//打开连接
 
-            MySqlCommand sqlCmd = new MySqlCommand("select * from users",sqlConnection);
-            MySqlDataReader reader = sqlCmd.ExecuteReader();
-            if(reader.HasRows)
-            {
-                while(reader.Read())
-                {
-                    string username = reader.GetString("username");
-                    string passward = reader.GetString("passward");
-                    Console.WriteLine(username + "  :  " + passward);
-                }
-            }
-            //关闭读取流以及连接
-            reader.Close();
+            #region 数据库查询
+            //MySqlCommand sqlCmd = new MySqlCommand("select * from users", sqlConnection);
+            //MySqlDataReader reader = sqlCmd.ExecuteReader();
+            //if (reader.HasRows)
+            //{
+            //    while (reader.Read())
+            //    {
+            //        string username = reader.GetString("username");
+            //        string passward = reader.GetString("passward");
+            //        Console.WriteLine(username + "  :  " + passward);
+            //    }
+            //}
+            ////关闭读取流以及连接
+            //reader.Close();
+            #endregion
+
+            #region 数据库插入
+            string username = "ddd";
+            string passward = "444";
+            string cmdTxt = "insert into users set username ='" + username + "',passward='" + passward + "'";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = sqlConnection;
+            cmd.CommandText = cmdTxt;
+            cmd.ExecuteNonQuery();
+            #endregion
+
             sqlConnection.Close();
 
             Console.ReadKey();
