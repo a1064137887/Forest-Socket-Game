23e487f6ec1fccfe6f487f555b17424d2fa5c8b9
YangHaoRan <1064137887@qq.com>
2018-07-21 11:36:59 +0800

    【简要描述】：初步撰写服务端跟客户端 【详细描述】： 	1.初步安排资源管理树 	2.初步撰写Client类 	3.初步撰写Server类，添加一个List存放Client 【修 改 人】：杨浩然

---
+++ 游戏服务器端/Server/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace GameServer.Server
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private Server server;
+        public Client() { }
+        public Client(Socket clientSocket,Server server)
+        {
+            this.clientSocket = clientSocket;
+            this.server = server;
+        }
+
+        public void Start()
+        {
+            clientSocket.BeginReceive(null, 0, 0, SocketFlags.None, ReceiveCallBack, null);
+        }
+
+        public void Close()
+        {
+            if (clientSocket == null) return;
+            clientSocket.Close();
+            //从Server类中的List中移除Client对象
+            server.RemoveClient(this);
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                int count = clientSocket.EndReceive(ar);
+                if(count == 0)
+                {
+                    Close();
+                }
+
+                //TODO：处理数据
+                clientSocket.BeginReceive(null, 0, 0, SocketFlags.None, ReceiveCallBack, null);//重新打开监听
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Close();
+            }
+        }
+ 
+
+    //class end
+    }
+
+//namespace end
+}
---
+++ 游戏服务器端/Server/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer.Server
+{
+    class Server
+    {
+        private Socket serverSocket;
+        private IPEndPoint ipEndPoint;
+        private List<Client> clientList;
+
+        public Server() { }
+        public Server(string ip,int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+        }
+
+        //通过ip和port设置该类的ipEndPoint
+        public void SetIpEndPoint(string ip,int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+        }
+
+        //开始连接
+        public void Start()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack,null);
+        }
+        
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+            Client client = new Client(clientSocket,this);
+            clientList.Add(client);
+        }
+
+        public void RemoveClient(Client client)
+        {
+            if (client == null) return;
+            //避免有多个Client同时调用该方法
+            lock(clientList)
+            {
+                clientList.Remove(client);
+            }
+        }
+
+    //class end
+    }
+
+//namespace end
+}
